2e2c96c2e4a0c3bca8681ed99d457f7d5a8709fc
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2025-06-19 19:52:44 +0530

    Time: 0 ms (100%), Space: 9.9 MB (97.1%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int rob(vector<int>& arr) {
+        int n=arr.size();
         int maxi=0;
-        if(arr.size()==1){return arr[0];}
-        if(arr.size()==2){return max(arr[0],arr[1]);}
-        if(arr.size()==3){
+        if(n==1){return arr[0];}
+        if(n==2){return max(arr[0],arr[1]);}
+        if(n==3){
             return max(arr[0]+arr[2],arr[1]);
         }
         int f=arr[0];
         int s=arr[1];
         int t=arr[2]+arr[0];
-        for(int i=3;i<arr.size();i++){
+        for(int i=3;i<n;i++){
            int  l=max(s+arr[i],f+arr[i]);
            f=s;
            s=t;
