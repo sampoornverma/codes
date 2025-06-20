27f9685427f9a10623446f98b2fb4aaf1158c5f3
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2025-06-20 23:34:21 +0530

    Time: 0 ms (100%), Space: 12 MB (28.27%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -3,7 +3,7 @@ public:
     int c(vector<vector<int>>& o,int m,int n,vector<vector<int>>& dp,int x,int y,int count){
         if(x==m-1 && y==n-1) return 1;
         if(x>=m || y>=n) return 0;
-        if(o[x][y]==1 || o[x][y]==1) return 0;
+        if(o[x][y]==1) return 0;
         if(dp[x][y]!= -1) return dp[x][y];
         int p=c(o,m,n,dp,x,y+1,count);
         int e=c(o,m,n,dp,x+1,y,count);
@@ -14,6 +14,8 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& o) {
         int m=o.size();
         int n=o[0].size();
+        if (o[0][0] == 1 || o[m - 1][n - 1] == 1) return 0;
+
         vector<vector<int>> dp(m,vector<int>(n,-1));
         int count=0;
         return c(o,m,n,dp,0,0,count);
