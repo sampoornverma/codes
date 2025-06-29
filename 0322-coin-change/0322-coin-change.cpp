7fcea1722436010c32703a55e2d5a18833dbb0bc
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2025-06-29 13:42:40 +0530

    Time: 37 ms (52.43%), Space: 48.8 MB (41.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int f(vector<int>& coins, int amount, vector<vector<int>>& dp, int i) {
-        if (amount == 0) return 0;               // Base: exact amount formed
-        if (amount < 0 || i >= coins.size()) return 1e9; // Base: not possible
-        if (dp[i][amount] != -1) return dp[i][amount];
-
-        // Pick the coin (stay on the same coin as we can reuse it)
-        int pick = 1 + f(coins, amount - coins[i], dp, i);
-
-        // Not pick the coin (move to next coin)
-        int notPick = f(coins, amount, dp, i + 1);
-
-        return dp[i][amount] = min(pick, notPick);
-    }
 
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        int ans = f(coins, amount, dp, 0);
-        return (ans >= 1e9 ? -1 : ans);  // If impossible, return -1
+        int n=coins.size();
+        const int INF = 1e9; 
+        vector< vector<int>> dp(n, vector<int>(amount +1,INF));
+        for (int i = 0; i < n; i++) dp[i][0] = 0;
+        for (int j = 1; j <= amount; j++) {
+            if (j % coins[0] == 0)
+                dp[0][j] = j / coins[0];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=amount;j++){
+                int notPick = dp[i - 1][j];
+                int pick = INF;
+                if (coins[i] <= j)
+                    pick = 1 + dp[i][j - coins[i]];
+                dp[i][j] = min(pick, notPick);
+            }
+        }
+        return (dp[n - 1][amount] == INF) ? -1 : dp[n - 1][amount];
+
+        
     }
 };
