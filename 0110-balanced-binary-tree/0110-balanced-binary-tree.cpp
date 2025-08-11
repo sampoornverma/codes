ca5ffc06ac1dc09933d3a341710b5edf86e473bb
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2025-08-12 02:20:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,24 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    // Calculates height of a subtree
-    int f(TreeNode* node){
-        if(node == nullptr) return 0;
-        return 1 + max(f(node->right), f(node->left));
+    int f(TreeNode* root){
+        if(root==nullptr)return 0;
+        return 1+ max(f(root->left),f(root->right));
     }
-
-    // Checks if a tree is height-balanced
     bool isBalanced(TreeNode* root) {
-        if(root == nullptr) return true;
+    if(root==nullptr)return true;
+    int a=0,b=0;
+    if(root->left != nullptr) a= f(root->left);
+    
+    if(root->right != nullptr) b= f(root->right);
+    
+    int c=abs(a-b);
+    if(c>1)return false;
+     return true;
 
-        int a = f(root->right);
-        int b = f(root->left);
-        int c = abs(a - b);
-
-        // Current node is balanced and both subtrees must also be balanced
-        if(c <= 1 && isBalanced(root->left) && isBalanced(root->right)) {
-            return true;
-        } else {
-            return false;
-        }
     }
 };
