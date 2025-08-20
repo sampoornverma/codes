261d2e0343ea73c57c3e398a2aee4a9b94706571
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2025-08-21 00:44:17 +0530

    Time: 13 ms (12.32%), Space: 19 MB (95.91%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
-    bool canFinish(int v, vector<vector<int>>& pre) {
-        vector<vector<int>> adj(v);  // initialize adj with size v
-
-        // Create adjacency list
-        for (int i = 0; i < pre.size(); i++) {
-            adj[pre[i][1]].push_back(pre[i][0]); // Reverse edge: course B → course A
+    bool canFinish(int v, vector<vector<int>>& edges) {
+    vector<vector<int>> adj(v);
+        for (int i = 0; i < edges.size(); i++) {
+            adj[edges[i][0]].push_back(edges[i][1]);
         }
 
+        // Compute indegrees
         vector<int> indegree(v, 0);
-        for (int i = 0; i < v; i++) {
-            for (int j : adj[i]) {
-                indegree[j]++;
-            }
+        for (int i = 0; i < edges.size(); i++) {
+            indegree[edges[i][1]]++;
         }
 
+        // Push all nodes with indegree 0
         queue<int> q;
         for (int i = 0; i < v; i++) {
-            if (indegree[i] == 0)
-                q.push(i);
+            if (indegree[i] == 0) q.push(i);
         }
 
-        int count = 0;
+        int count = 0; // count nodes in topo sort
         while (!q.empty()) {
             int node = q.front();
             q.pop();
             count++;
-            for (int neigh : adj[node]) {
-                indegree[neigh]--;
-                if (indegree[neigh] == 0)
-                    q.push(neigh);
+
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0) q.push(it);
             }
         }
 
-        return count == v; // All courses processed → No cycle
+        // If count < v, cycle exists
+        return (count == v);   
     }
 };
