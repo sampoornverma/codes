248132d2e9cefdaf8fc915197240aa774bbebe8a
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2025-09-10 19:25:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int size = s.size();
-        int n = 0;
-        int a = 1;
         int count = 0;
-        long long num = 0;
-
-        while (n < size) {
-            if (s[n] == '+' || s[n] == '-') {
-                if (count == 0) {
-                    a = (s[n] == '-') ? -1 : 1;
-                    count = 1;
-                } else break;
-            } else if (isdigit(s[n])) {
-                num = num * 10 + (s[n] - '0');
-                count = 1;
-
-                // Clamp early to avoid overflow
-                if (a * num <= INT_MIN) return INT_MIN;
-                if (a * num >= INT_MAX) return INT_MAX;
-
-            } else if (count > 0) {
-                break;
-            } else if (s[n] != ' ') {
-                break;
+        string s1 = "";
+        char sign = '+';
+        
+        for (char c : s) {
+            if (!count && c != ' ') {
+                if (c == '+' || c == '-') {
+                    sign = c;
+                    count++;
+                    continue; // skip storing sign into s1
+                }
+                count++;
             }
-            n++;
+            
+            if (isalpha(c) || (count && c == ' ') || (count && c == '+') || (count && c == '-')) {
+                break;
+            } else {
+                s1 = s1 + c;
+            }
         }
-
-        return a * num;
+        
+        if (s1 == "") return 0;
+        
+        long long final = 0; // use long long to check overflow
+        for (char c : s1) {
+            int a = c - '0';
+            final = final * 10 + a;
+            
+            if (sign == '+' && final > INT_MAX) return INT_MAX;
+            if (sign == '-' && -final < INT_MIN) return INT_MIN;
+        }
+        
+        return (sign == '-') ? -final : final;
     }
 };
