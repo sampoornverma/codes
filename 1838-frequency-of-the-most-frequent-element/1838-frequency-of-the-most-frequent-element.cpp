dedf47584dcbcbf8341b40c6683201d80774edd3
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2025-09-14 02:38:11 +0530

    Time: 35 ms (76.27%), Space: 105.9 MB (29.83%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        long long total = 0;
+        long long total = 0;           // sum of elements in window [i..j]
         int ans = 0;
-        int l = 0;
+        int j = nums.size() - 1;       // right pointer (largest in window)
 
-        for (int r = 0; r < nums.size(); r++) {
-            total += nums[r];
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            total += nums[i];
 
-            // While cost > k, shrink window from left
-            while ((long long)nums[r] * (r - l + 1) - total > k) {
-                total -= nums[l];
-                l++;
+            // target is nums[j] (largest in window [i..j])
+            while (j >= i && (long long)nums[j] * (j - i + 1) - total > k) {
+                total -= nums[j];
+                j--;
             }
 
-            ans = max(ans, r - l + 1);
+            if (j >= i) ans = max(ans, j - i + 1);
         }
 
         return ans;
