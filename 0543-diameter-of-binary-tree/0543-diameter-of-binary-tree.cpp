7bc1c19b421594cfaef425310943e148dc4e92ff
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2026-02-01 13:01:05 +0530

    Time: 0 ms (100%), Space: 23.7 MB (42.31%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int depth(TreeNode* node, int &diameter) {
-        if (node == nullptr) return 0;
+    int maxi = 0;
 
-        int leftDepth = depth(node->left, diameter);
-        int rightDepth = depth(node->right, diameter);
+    int f(TreeNode* root) {
+        if (root == nullptr) return 0;
 
-        // Update the diameter if the path through this node is longer
-        diameter = max(diameter, leftDepth + rightDepth);
+        int lh = f(root->left);
+        int rh = f(root->right);
 
-        // Return height of the current node
-        return 1 + max(leftDepth, rightDepth);
+        maxi = max(maxi, lh + rh);
+
+        return 1 + max(lh, rh);
     }
+
     int diameterOfBinaryTree(TreeNode* root) {
-        int diameter = 0;
-        depth(root, diameter);
-        return diameter;
+        f(root);
+        return maxi;
     }
-
-
-    
 };
