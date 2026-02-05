a45144a47fffafa440ed2dbbe046bc0b81459c30
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2026-02-05 11:50:32 +0530

    Time: 10 ms (22.28%), Space: 25.4 MB (98.89%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n);
+
+        for (int i = 0; i < n; i++) {
+            int idx = (i + nums[i]) % n;
+            if (idx < 0) idx += n;   // handle negative modulo
+            res[i] = nums[idx];
+        }
+
+        return res;
+    }
+};
