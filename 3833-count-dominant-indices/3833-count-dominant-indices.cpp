a7ad6a56c5f535086df4233b0c7f91dad4b4b717
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2026-02-09 19:42:12 +0530

    Time: 0 ms (100%), Space: 32.4 MB (0%) - LeetHub

--- 3833-count-dominant-indices/3833-count-dominant-indices.cpp
+++ 3833-count-dominant-indices/3833-count-dominant-indices.cpp
@@ -3,12 +3,12 @@ public:
     int dominantIndices(vector<int>& nums) {
     if(nums.size()==1)return 0;
     int sum=0;
-    int count=0;
     int count1=0;
+
     for(int i=nums.size()-2;i>=0;i--){
         sum=sum+nums[i+1];
-        count++;
-        if(nums[i]>sum/count){
+        
+        if(nums[i]>sum/(nums.size()-i-1)){
             count1++;
         }
     }
