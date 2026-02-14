afd0b0197c81bccdd8d295401d77dc52fdebaf2d
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2026-02-14 16:08:10 +0530

    Time: 0 ms (100%), Space: 18.3 MB (44.26%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& ans, int sr, int sc, int color,int init, vector<int> gr, vector<int> gc){
-        ans[sr][sc]=color;
-        int n=ans.size();
-        int m=ans[0].size();
-        for(int i=0;i<4;i++){
-            int r=sr+gr[i];
-            int c=sc+gc[i];
-            if(((c>=0 &&r>=0) && (r<n && c<m))&&(ans[r][c]!=color && ans[r][c]==init)){
-                dfs(ans,r,c,color,init,gr,gc);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+
+        int n = image.size();
+        int m = image[0].size();
+        int original = image[sr][sc];
+
+        // Important edge case
+        if (original == color) return image;
+
+        vector<int> gr = {-1, 0, 1, 0}; 
+        vector<int> gc = {0, 1, 0, -1};
+
+        queue<pair<int,int>> q;
+        q.push({sr, sc});
+
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+
+            int x = it.first;
+            int y = it.second;
+
+            image[x][y] = color;
+
+            for (int i = 0; i < 4; i++) {
+                int a = x + gr[i];
+                int b = y + gc[i];
+
+                if (a >= 0 && a < n && b >= 0 && b < m && image[a][b] == original) {
+                    q.push({a, b});
+                }
             }
         }
 
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int initalcol=image[sr][sc];
-        vector<vector<int>> ans=image;
-        vector<int> gr={-1,0,1,0};
-        vector<int>  gc={0,1,0,-1};
-        dfs(ans,sr,sc,color,initalcol,gr,gc);
-        return ans;
+        return image;
     }
 };
