2340c6970d356ab7ff0232ce514d141abbde3a08
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2026-02-15 22:28:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,58 +1,58 @@
-class DSU {
-public:
-    vector<int> rank, parent;
-    DSU(int n) {
-        rank.resize(n+1, 0);
-        parent.resize(n+1);
-        for (int i = 0; i <= n; i++) parent[i] = i;
-    }
-
-    int findparent(int n) {
-        if (n == parent[n]) return n;
-        return parent[n] = findparent(parent[n]);
-    }
-
-    void unionByRank(int u, int v) {
-        int up = findparent(u);
-        int vp = findparent(v);
-        if (up == vp) return;
-        if (rank[up] < rank[vp]) {
-            parent[up] = vp;
-        } else if (rank[up] > rank[vp]) {
-            parent[vp] = up;
-        } else {
-            parent[vp] = up;
-            rank[up]++;
-        }
-    }
-};
-
 class Solution {
 public:
-    int removeStones(vector<vector<int>>& stones) {
-        int maxRow = 0, maxCol = 0;
-        for (auto &s : stones) {
-            maxRow = max(maxRow, s[0]);
-            maxCol = max(maxCol, s[1]);
+
+    void dfs(vector<vector<int>>& vis, vector<vector<int>>& a, int i, int j) {
+
+        int n = a.size();
+        int m = a[0].size();
+
+        vis[i][j] = 1;
+
+        // check all stones in same row
+        for (int col = 0; col < m; col++) {
+            if (a[i][col] == 1 && !vis[i][col]) {
+                dfs(vis, a, i, col);
+            }
         }
 
-        int offset = maxRow + 1;
-        DSU ds(maxRow + maxCol + 2);
+        // check all stones in same column
+        for (int row = 0; row < n; row++) {
+            if (a[row][j] == 1 && !vis[row][j]) {
+                dfs(vis, a, row, j);
+            }
+        }
+    }
 
-        // Union rows and columns for each stone
-        for (auto &s : stones) {
-            int rowNode = s[0];
-            int colNode = s[1] + offset;
-            ds.unionByRank(rowNode, colNode);
+
+    int removeStones(vector<vector<int>>& s) {
+
+        int maxr = 0, maxc = 0;
+
+        for (auto &x : s) {
+            maxr = max(maxr, x[0]);
+            maxc = max(maxc, x[1]);
         }
 
-        // Count unique connected components
-        unordered_set<int> comps;
-        for (auto &s : stones) {
-            int node = ds.findparent(s[0]);  // row representative is enough
-            comps.insert(node);
+        // grid
+        vector<vector<int>> a(maxr + 1, vector<int>(maxc + 1, 0));
+
+        for (auto &x : s) {
+            a[x[0]][x[1]] = 1;
         }
 
-        return stones.size() - comps.size(); 
+        vector<vector<int>> vis(maxr + 1, vector<int>(maxc + 1, 0));
+
+        int components = 0;
+
+        for (auto &x : s) {
+            int i = x[0], j = x[1];
+
+            if (!vis[i][j]) {
+                components++;
+                dfs(vis, a, i, j);
+            }
+        }
+
+        return s.size() - components;
     }
 };
