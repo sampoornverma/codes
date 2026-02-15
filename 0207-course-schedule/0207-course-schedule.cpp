fec6b42c971ae0bcee8d296de87c46ebef43ac86
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2026-02-15 20:34:19 +0530

    Time: 12 ms (17.22%), Space: 19.3 MB (67.97%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    // 0 = unvisited, 1 = visiting, 2 = visited
-    bool dfs(int node, vector<vector<int>>& adj, vector<int>& vis) {
-        vis[node] = 1;  // mark as visiting
-
-        for (int nei : adj[node]) {
-            if (vis[nei] == 1) {
-                // cycle detected
-                return false;
-            }
-            if (vis[nei] == 0) {
-                if (!dfs(nei, adj, vis))
-                    return false;
-            }
-        }
-
-        vis[node] = 2;  
-        return true;
-    }
-
     bool canFinish(int v, vector<vector<int>>& pre) {
-        vector<vector<int>> adj(v);
-        vector<int> vis(v, 0);
+        vector<vector<int>> adj(v);  // initialize adj with size v
 
+        // Create adjacency list
         for (int i = 0; i < pre.size(); i++) {
-            adj[pre[i][1]].push_back(pre[i][0]);
+            adj[pre[i][1]].push_back(pre[i][0]); // Reverse edge: course B → course A
         }
 
+        vector<int> indegree(v, 0);
         for (int i = 0; i < v; i++) {
-            if (vis[i] == 0) {
-                if (!dfs(i, adj, vis))
-                    return false;
+            for (int j : adj[i]) {
+                indegree[j]++;
             }
         }
-        return true;
+
+        queue<int> q;
+        for (int i = 0; i < v; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        int count = 0;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            count++;
+            for (int neigh : adj[node]) {
+                indegree[neigh]--;
+                if (indegree[neigh] == 0)
+                    q.push(neigh);
+            }
+        }
+
+        return count == v; // All courses processed → No cycle
     }
 };
