f748e1a61216bae11fa56d22dfefb7560ccc2809
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2026-02-17 20:17:52 +0530

    Time: 5 ms (36.76%), Space: 17.6 MB (87.08%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>>> adj(n);
-        for(auto &f : flights){
-            adj[f[0]].push_back({f[1], f[2]});
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+        
+        vector<pair<int,int>> adj[n];
+
+        // build graph
+        for (auto &it : flights) {
+            adj[it[0]].push_back({it[1], it[2]});
         }
 
-        queue<pair<int,pair<int,int>>> pq;
-        pq.push({0, {src, 0}});
+        // {stops, {node, cost}}
+        queue<pair<int, pair<int,int>>> q;
+
+        q.push({0, {src, 0}});
 
         vector<int> dist(n, 1e9);
         dist[src] = 0;
 
-        while(!pq.empty()){
-            auto it = pq.front();
-            pq.pop();
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+
             int stops = it.first;
-            int u = it.second.first;
-            int v= it.second.second;
-            if(stops>k) continue;
-            for(auto t : adj[u]){
-            int a= t.first;
-            int b=t.second;
-            if(v+b<dist[a] && stops<=k){
-                dist[a]=v+b;
-                pq.push({stops+1,{a,v+b}});
-            }
+            int node = it.second.first;
+            int cost = it.second.second;
+
+            if (stops > K) continue;
+
+            for (auto iter : adj[node]) {
+                int adjNode = iter.first;
+                int edW = iter.second;
+
+                if (cost + edW < dist[adjNode] && stops <= K) {
+                    dist[adjNode] = cost + edW;
+                    q.push({stops + 1, {adjNode, cost + edW}});
+                }
             }
         }
 
-        if(dist[dst]==1e9)return -1;
-        else return dist[dst];
+        if (dist[dst] == 1e9) return -1;
+        return dist[dst];
     }
 };
