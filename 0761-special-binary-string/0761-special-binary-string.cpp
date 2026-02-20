b85ef95a6fbb2135f8194aa55834d7b5378a6afb
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2026-02-20 10:02:13 +0530

    Time: 3 ms (21.49%), Space: 9.4 MB (34.67%) - LeetHub

--- 0761-special-binary-string/0761-special-binary-string.cpp
+++ 0761-special-binary-string/0761-special-binary-string.cpp
@@ -4,14 +4,28 @@ public:
     int count = 0, start = 0;
     vector<string> subs;
     for (int i = 0; i < s.size(); ++i) {
-        count += (s[i] == '1') ? 1 : -1;
-        if (count == 0) {
-            string sub = s.substr(start, i - start + 1);
-            sub = "1" + makeLargestSpecial(sub.substr(1, sub.size() - 2)) + "0";
-            subs.push_back(sub);
-            start = i + 1;
-        }
+    if (s[i] == '1') {
+        count += 1;
+    } else {
+        count -= 1;
     }
+
+    // Found a "good" substring
+    if (count == 0) {
+        int length = i - start + 1;
+        string sub = s.substr(start, length);
+
+        // Remove outer '1' and '0', recursively sort the inner part
+        string inner = sub.substr(1, length - 2);
+        string processed = makeLargestSpecial(inner);
+
+        // Wrap it back with '1' and '0'
+        subs.push_back("1" + processed + "0");
+
+        // Move to the next segment
+        start = i + 1;
+    }
+}
     sort(subs.begin(), subs.end(), greater<string>());
     string result;
     for (auto &str : subs) result += str;
