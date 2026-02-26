61023d9d4eb79c98ce261fd26d6372ff862dd756
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2026-02-27 01:01:05 +0530

    Time: 0 ms (100%), Space: 13.4 MB (23.51%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-      int n = triangle.size();
-    vector<int> dp(triangle.back());  // Start with the last row
+    int minimumTotal(vector<vector<int>>& t) {
+        int n = t.size();
+        int m = t[n-1].size();
 
-    for (int i = n - 2; i >= 0; --i) {
-        for (int j = 0; j < triangle[i].size(); ++j) {
-            dp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
+        vector<vector<int>> dp(n, vector<int>(m, 1e9));
+        dp[0][0] = t[0][0];
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < t[i].size(); j++){
+                if(i == 0 && j == 0) continue;
+
+                int left = 1e9;
+                if(j-1 >= 0)
+                    left = dp[i-1][j-1];
+
+                int top = 1e9;
+                if(j < t[i-1].size())
+                    top = dp[i-1][j];
+
+                dp[i][j] = t[i][j] + min(top, left);
+            }
         }
-    }
-    return dp[0];  
+
+        int maxi = 1e9;
+        for(int i = 0; i < m; i++){
+            maxi = min(maxi, dp[n-1][i]);
+        }
+        return maxi;
     }
 };
