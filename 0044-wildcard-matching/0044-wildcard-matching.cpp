775baefc91256dbc6da9f91fdc50536375c5423a
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2026-03-30 02:13:16 +0530

    Time: 60 ms (34.31%), Space: 15.9 MB (51.9%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    bool match(string &s, string &p, int i, int j, vector<vector<int>>& dp) {
-        if (i < 0 && j < 0) return true;          // Both strings exhausted
-        if (i < 0) {
-            for (int k = 0; k <= j; k++) {
-                if (p[k] != '*') return false;
-            }
-            return true;
-        }
-        if (j < 0) return false;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (s[i] == p[j] || p[j] == '?') {
-            return dp[i][j] = match(s, p, i - 1, j - 1, dp);
-        } else if (p[j] == '*') {
-            // Two choices: '*' matches current char OR it doesn't
-            return dp[i][j] = match(s, p, i - 1, j, dp) || match(s, p, i, j - 1, dp);
-        } else {
-            return dp[i][j] = false;
-        }
-    }
-
     bool isMatch(string s, string p) {
+
         int n = s.size();
         int m = p.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return match(s, p, n - 1, m - 1, dp);
+
+        vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
+
+        dp[0][0] = true;
+
+        for(int j=1; j<=m; j++)
+        {
+            if(p[j-1]=='*')
+                dp[0][j] = dp[0][j-1];
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+
+                if(p[j-1]=='?' || s[i-1]==p[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+
+                else if(p[j-1]=='*')
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+
+                else
+                    dp[i][j] = false;
+            }
+        }
+
+        return dp[n][m];
     }
 };
