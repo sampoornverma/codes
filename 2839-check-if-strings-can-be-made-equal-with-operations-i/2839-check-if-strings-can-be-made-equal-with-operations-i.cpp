f74a93b3de1d17189acd452e6e489c2e8f1799fd
sampoornverma <143827501+sampoornverma@users.noreply.github.com>
2026-03-29 11:59:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,10 +1,32 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        bool s02=s1[0]==s2[2] && s1[2]==s2[0];
-        bool b02=s1[0]==s2[0] && s1[2]==s2[2];
-        bool s13=s1[1]==s2[3] && s1[3]==s2[1];
-        bool b13=s1[1]==s2[1] && s1[3]==s2[3];
-        return ((s02 ||b02)&&(s13 || b13));
+
+
+        string eve1="";
+        string eve2="";
+        string odd1="";
+        string odd2="";
+        int n=s1.size();
+        int i=0;
+        while(i<n){
+            eve1=eve1+s1[i];
+            eve2=eve2+s2[i];
+            i+=2;
+        }
+        i=1;
+        while(i<n){
+            odd1=odd1+s1[i];
+            odd2=odd2+s2[i];
+            i+=2;
+        }
+        sort(eve1.begin(),eve1.end());
+        sort(eve2.begin(),eve2.end());
+        sort(odd1.begin(),odd1.end());
+        sort(odd2.begin(),odd2.end());
+        if(eve1==eve2 && odd1==odd2){
+            return true;
+        }
+        return false;
     }
 };
