42f1904cbe7b82deb8976633654d3271f407ab28
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-06-01 11:57:31 +0530

    Time: 0 ms (100%), Space: 14.4 MB (21.54%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -4,8 +4,6 @@ public:
         sort(cost.begin(),cost.end());
         reverse(cost.begin(),cost.end());
         int n=cost.size();
-        if(n==1)return cost[0];
-        if(n==2)return cost[0]+cost[1];
         int total=0;
         for(int i=0;i<n;i++){
             if((i+1)%3!=0) total=total+cost[i];
