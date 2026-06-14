e6d620c576312c71b955b7adb51d26d913d35216
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-06-14 21:39:16 +0530

    Time: 4 ms (56.7%), Space: 124.3 MB (67.45%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,34 +1,38 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        int count=0;
-        ListNode *h=head;
-        while(h!=nullptr){
-            count++;
-            h=h->next;
-        }
-        h=head;
-        vector<int> ans;
 
-        while(h!=nullptr){
-            ans.push_back(h->val);
-            h=h->next;
+        // Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        int n=count;
-        int maxi=0;
-        for(int i=0;i<n/2;i++){
-            maxi=max(maxi,ans[i]+ans[n-1-i]);
+
+        // Reverse second half
+        ListNode* prev = nullptr;
+        ListNode* curr = slow;
+
+        while (curr) {
+            ListNode* nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
         }
-        return maxi;
+
+        int ans = 0;
+
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while (second) {
+            ans = max(ans, first->val + second->val);
+            first = first->next;
+            second = second->next;
+        }
+
+        return ans;
     }
 };
