bab0d190f574fc3938e9ef55765eb9d7ae44b447
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-06-16 20:28:02 +0530

    Time: 44 ms (75%), Space: 29.2 MB (46.1%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    res.push_back({nums[i], nums[left], nums[right]});
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left=i+1;
+            int right=n-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                 if(sum<0)left++;
+                else if(sum>0)right--;
+                else{
+                    ans.push_back({nums[i],nums[left],nums[right]});
                     left++;
                     right--;
-
-                    // Skip duplicates for both sides
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                } 
-                else if (sum < 0) {
-                    left++;
-                } 
-                else {
-                    right--;
+                while(left < right && nums[left]==nums[left-1])left++;
+                while(left < right && nums[right]==nums[right+1])right--;
                 }
             }
         }
 
-        return res;
+        return ans;
     }
 };
