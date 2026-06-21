24b8ec5db7bf723480ba3ea111f277dca802de4e
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-06-21 11:56:58 +0530

    Time: 4 ms (92.11%), Space: 80.9 MB (29.03%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,15 +1,33 @@
+constexpr int M = 1e5 + 1;
+int freq[M];
 class Solution {
 public:
-    int maxIceCream(vector<int>& costs, int coins) {
-        int n=costs.size();
-        sort(costs.begin(),costs.end());
-        int count=0;
-        for(int i=0;i<n;i++){
-            
-            coins=coins-costs[i];
-            if(coins<0)return count;
-            count++;
+    static int maxIceCream(vector<int>& costs, int coins) {
+        int xMax = 0;
+        for (int x : costs) {
+            freq[x]++;
+            xMax = max(xMax, x);
         }
-        return n;
+        int cnt = 0;
+        for (int x = 1; x <= xMax; x++) {
+            const int f = freq[x];
+            if (f == 0)
+                continue;
+            int buy = min(coins / x, f);
+            if (buy == 0)
+                break;
+            cnt += buy;
+            coins -= buy * x;
+        }
+        // reset for the next testcase
+        for (int x : costs)
+            freq[x] = 0;
+        return cnt;
     }
 };
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
