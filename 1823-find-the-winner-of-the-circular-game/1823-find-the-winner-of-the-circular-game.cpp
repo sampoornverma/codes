bf26fc99b9eba13ac700f78d8b9961c639784214
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-06-25 04:23:58 +0530

    Time: 0 ms (100%), Space: 8 MB (64.99%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
+    int helper(int n,int k){
+        if(n==1)
+            return 0;
+        return (helper(n-1,k) + k) % n;
+    }
     int findTheWinner(int n, int k) {
-        queue<int> q;
-        for(int i=1;i<=n;i++){
-            q.push(i);
-        }
-        while(q.size()!=1){
-            for(int i=0;i<k-1;i++){
-                int l=q.front();
-                q.pop();
-                q.push(l);
-            }
-            q.pop();
-        }
-        return q.front();
+        return helper(n,k)+1;   //+1 is for converting 0-based indexing to 1-based indexing
     }
 };
