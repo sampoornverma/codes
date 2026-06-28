b2c5c6708ffd5f33261caf7c2773c972d30026f5
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-06-28 07:04:25 +0530

    Time: 11 ms (41.24%), Space: 55.2 MB (69.21%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -4,9 +4,6 @@ public:
         sort(arr.begin(),arr.end());
         if(arr[0]!=1)arr[0]=1;
         int n=arr.size();
-        if(n==1)return arr[0];
-        int mini=0;
-        int x=1;
         for(int i=1;i<n;i++){
             if(arr[i]>arr[i-1])arr[i]=arr[i-1]+1;
         }
