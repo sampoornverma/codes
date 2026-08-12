015bd25584b42c5996e68f17be16d30d9c8810cd
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-08-12 12:12:20 +0530

    Time: 0 ms (100%), Space: 8.7 MB (60.43%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    bool c(string s){
-        string a = s;
-        reverse(a.begin(), a.end());
-        return a == s;
-    }
-
     int countSubstrings(string s) {
+        int n = s.size();
         int count = 0;
 
-        for(int i = 0; i < s.size(); i++){
-            for(int j = i; j < s.size(); j++){
-                string ans = s.substr(i, j - i + 1);
-                if(c(ans)) count++;
+        for(int center = 0; center < n; center++) {
+
+            int l = center, r = center;
+
+            while(l >= 0 && r < n && s[l] == s[r]) {
+                count++;
+                l--;
+                r++;
+            }
+
+            l = center;
+            r = center + 1;
+
+            while(l >= 0 && r < n && s[l] == s[r]) {
+                count++;
+                l--;
+                r++;
             }
         }
+
         return count;
     }
 };
