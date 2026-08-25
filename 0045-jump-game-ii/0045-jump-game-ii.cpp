a39ee84b63e399da6e978e528ec4431fd67bbcfd
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-08-25 16:32:33 +0530

    Time: 62 ms (19.94%), Space: 21.8 MB (15.18%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n=nums.size();
-        vector<int> arr(n,1000000);
+        vector<int> arr(n,100000);
         arr[0]=0;
         for(int i=0;i<n;i++){
             for(int j=1;j<=nums[i] && i+j<n;j++){
