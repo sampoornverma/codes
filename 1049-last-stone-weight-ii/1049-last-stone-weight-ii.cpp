37aa97f48b1f0cef4d8fdfdec90f56e3b8daf091
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-08-27 18:40:59 +0530

    Time: 0 ms (100%), Space: 11.6 MB (53.42%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int sum = 0;
-    vector<vector<int>> dp;
-    int f(int i, int total, vector<int>& stones) {
-        if (i == stones.size()) {
-            return abs(sum - 2 * total);
-        }
-        if (dp[i][total] != -1)
-            return dp[i][total];
-        int take = f(i + 1, total + stones[i], stones);
-        int notTake = f(i + 1, total, stones);
-        return dp[i][total] = min(take, notTake);
-    }
-
     int lastStoneWeightII(vector<int>& stones) {
-        sum = 0;
-        for (int x : stones)
+        int sum = 0;
+
+        for (int x : stones) {
             sum += x;
-        int n = stones.size();
-        dp.assign(n, vector<int>(sum + 1, -1));
-        return f(0, 0, stones);
+        }
+
+        int target = sum / 2;
+
+        vector<int> dp(target + 1, 0);
+
+        for (int stone : stones) {
+            for (int j = target; j >= stone; j--) {
+                dp[j] = max(dp[j], dp[j - stone] + stone);
+            }
+        }
+
+        return sum - 2 * dp[target];
     }
 };
