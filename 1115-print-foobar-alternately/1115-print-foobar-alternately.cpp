1b001e12f8401eb8ef30f5505c933ea058a367ef
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-08-29 00:56:41 +0530

    Time: 3 ms (89.92%), Space: 11.3 MB (44.08%) - LeetHub

---
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.cpp
@@ -0,0 +1,44 @@
+class FooBar {
+private:
+    int n;
+    mutex mtx;
+    condition_variable cv;
+    bool fooTurn = true;
+
+public:
+    FooBar(int n) {
+        this->n = n;
+    }
+
+    void foo(function<void()> printFoo) {
+        for (int i = 0; i < n; i++) {
+            unique_lock<mutex> lock(mtx);
+
+            cv.wait(lock, [this]() {
+                return fooTurn;
+            });
+
+            // printFoo() outputs "foo". Do not change or remove this line.
+            printFoo();
+
+            fooTurn = false;
+            cv.notify_one();
+        }
+    }
+
+    void bar(function<void()> printBar) {
+        for (int i = 0; i < n; i++) {
+            unique_lock<mutex> lock(mtx);
+
+            cv.wait(lock, [this]() {
+                return !fooTurn;
+            });
+
+            // printBar() outputs "bar". Do not change or remove this line.
+            printBar();
+
+            fooTurn = true;
+            cv.notify_one();
+        }
+    }
+};
