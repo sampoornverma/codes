681d8b09472d05769b4f36427b38cdcca3732f5c
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-08-31 20:52:38 +0530

    Time: 406 ms (41.83%), Space: 211.4 MB (51.29%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-
-    int f(int i, int buy, int no, vector<int>& prices, vector<vector<vector<int>>>& dp){
-
-        if(i == prices.size() || no == 2)
-            return 0;
-
-        if(dp[i][buy][no] != -1)
-            return dp[i][buy][no];
-
-        int profit = 0;
-
-        if(buy == 0) // can buy
-        {
-            profit = max(
-                -prices[i] + f(i+1,1,no,prices,dp),
-                f(i+1,0,no,prices,dp)
-            );
-        }
-        else // must sell
-        {
-            profit = max(
-                prices[i] + f(i+1,0,no+1,prices,dp),
-                f(i+1,1,no,prices,dp)
-            );
-        }
-
-        return dp[i][buy][no] = profit;
-    }
-
     int maxProfit(vector<int>& prices) {
 
         int n = prices.size();
+        int k=2;
 
-        vector<vector<vector<int>>> dp(n,
-            vector<vector<int>>(2, vector<int>(3,-1)));
+        vector<vector<vector<int>>> dp(
+            n + 1,
+            vector<vector<int>>(2, vector<int>(k + 1, 0))
+        );
 
-        return f(0,0,0,prices,dp);
+        // dp[n][*][*] = 0 already
+        // dp[*][*][0] = 0 already
+
+        for (int i = n - 1; i >= 0; i--) {
+
+
+                for (int cap = 1; cap <= k; cap++) {
+
+                  
+
+                        dp[i][0][cap] = max(
+                            dp[i + 1][0][cap],                  // Skip
+                            -prices[i] + dp[i + 1][1][cap]      // Buy
+                        );
+
+                    
+
+                        dp[i][1][cap] = max(
+                            dp[i + 1][1][cap],                  // Hold
+                            prices[i] + dp[i + 1][0][cap - 1]   // Sell
+                        );
+                    
+                
+            }
+        }
+
+        return dp[0][0][k];
     }
 };
