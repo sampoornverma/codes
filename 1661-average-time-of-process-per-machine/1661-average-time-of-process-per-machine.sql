e8def8cb0a21b9dc5577b05ec1b6255911c0db62
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-09-01 13:26:13 +0530

    Time: 248 ms (83.38%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -3,7 +3,7 @@ SELECT A.machine_id,
 FROM Activity A
 JOIN Activity B
 ON A.machine_id = B.machine_id 
-AND A.process_id = B.process_id
+where A.process_id = B.process_id
 AND A.activity_type = 'start' 
 AND B.activity_type = 'end'
 GROUP BY A.machine_id;
