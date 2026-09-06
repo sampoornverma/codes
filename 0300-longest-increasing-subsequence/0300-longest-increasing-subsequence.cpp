45cafe787c35f82ab547720c13115a4f47eaa7c5
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-09-07 02:59:20 +0530

    Time: 1 ms (81.3%), Space: 14 MB (94.74%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int f(int i, vector<int>& nums, int p, vector<vector<int>>& dp){
-        int n = nums.size();
-        
-        if(i == n) return 0;
-
-        if(dp[i][p+1] != -1) 
-            return dp[i][p+1];
-
-        int notTake = f(i+1, nums, p, dp);
-
-        int take = 0;
-        if(p == -1 || nums[i] > nums[p])
-            take = 1 + f(i+1, nums, i, dp);
-
-        return dp[i][p+1] = max(take, notTake);
-    }
-
+    /* Function to find the longest increasing 
+    subsequence in the given array */
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-
-        return f(0, nums, -1, dp);
-    }
+        
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        
+        for(int i=1; i < n; i++) {
+                        if(nums[i] > temp.back()) 
+                temp.push_back(nums[i]); 
+                
+            else {
+                int ind = lower_bound(temp.begin(), temp.end(), nums[i]) -
+                          temp.begin();
+                          
+                temp[ind] = nums[i];
+            }    
+            
+        }
+                return temp.size();
+    }    
 };
