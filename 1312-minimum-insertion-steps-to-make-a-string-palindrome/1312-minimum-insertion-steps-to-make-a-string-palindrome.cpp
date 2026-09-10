80b9193d596c60f16d7fd49dc25c6d5b52a4bb79
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-09-11 00:34:46 +0530

    Time: 32 ms (57.4%), Space: 31.8 MB (15.83%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-int f(string& i, string& j,int n,int m,vector<vector<int>> &dp){
-        if(n==0 || m==0){
-            return 0;
+    int minInsertions(string s) {
+        int n=s.size();
+        string p=s;
+        reverse(p.begin(),p.end());
+        int m=p.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==p[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
         }
-        if(dp[n][m] != -1) return dp[n][m];
-        if(i[n-1]==j[m-1])return dp[n][m]= 1+f(i,j,n-1,m-1,dp);
-        else{
-            return dp[n][m]= max(f(i,j,n-1,m,dp),f(i,j,n,m-1,dp));
-        }
-    }
-    int minInsertions(string text1) {
-        string text2 = text1;
-    reverse(text2.begin(), text2.end());  // STL reverse instead of undefined reverseString
-    int n = text1.size();
-    int m = text2.size();
-    
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-    int x= f(text1, text2, n, m, dp);
-    return n-x;
+        return n-dp[n][m];
     }
 };
