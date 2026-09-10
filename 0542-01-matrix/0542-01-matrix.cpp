b72967e68fd602e648b0958ad672d00d33e38342
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-09-10 17:32:27 +0530

    Time: 19 ms (55.41%), Space: 34.4 MB (96.07%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,50 +1,50 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& m1) {
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
 
-        int n = m1.size();
-        int m = m1[0].size();
+        int n = mat.size();
+        int m = mat[0].size();
 
-        // {row, {col, distance}}
-        queue<pair<int, pair<int,int>>> q;
+        queue<pair<int, int>> q;
 
-        // Step 1: push all zeros and mark others as -1
+        // 1. Put all 0s into the queue
+        //    and mark 1s as unvisited (-1)
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (m1[i][j] == 0) {
-                    q.push({i, {j, 0}});
+
+                if (mat[i][j] == 0) {
+                    q.push({i, j});
                 } else {
-                    m1[i][j] = -1;
+                    mat[i][j] = -1;
                 }
             }
         }
 
-        vector<int> gr = {-1, 0, 1, 0}; 
-        vector<int> gc = {0, 1, 0, -1};
+        vector<int> dr = {-1, 0, 1, 0};
+        vector<int> dc = {0, 1, 0, -1};
 
-        // Step 2: BFS
+        // 2. Multi-source BFS
         while (!q.empty()) {
 
-            auto it = q.front();
+            auto [r, c] = q.front();
             q.pop();
-
-            int a = it.first;
-            int b = it.second.first;
-            int dist = it.second.second;
 
             for (int i = 0; i < 4; i++) {
 
-                int x = a + gr[i];
-                int y = b + gc[i];
+                int nr = r + dr[i];
+                int nc = c + dc[i];
 
-                if (x >= 0 && x < n && y >= 0 && y < m && m1[x][y] == -1) {
+                if (nr >= 0 && nr < n &&
+                    nc >= 0 && nc < m &&
+                    mat[nr][nc] == -1) {
 
-                    m1[x][y] = dist + 1;
-                    q.push({x, {y, dist + 1}});
+                    mat[nr][nc] = mat[r][c] + 1;
+
+                    q.push({nr, nc});
                 }
             }
         }
 
-        return m1;
+        return mat;
     }
 };
