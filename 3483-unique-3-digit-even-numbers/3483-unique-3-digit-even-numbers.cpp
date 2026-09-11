51f6fa6b0da75ea16775aa5e999f95cc0f75c3ac
sampoorn verma <143827501+sampoornverma@users.noreply.github.com>
2026-09-11 16:59:08 +0530

    Time: 11 ms (42.35%), Space: 30.9 MB (66.84%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        int count = 0;
+        int n = digits.size();
+
+        for(int i = 0; i < n; i++) {
+            if(digits[i] == 0) continue; // hundreds digit cannot be 0
+
+            for(int j = 0; j < n; j++) {
+                if(j == i) continue;
+
+                for(int k = 0; k < n; k++) {
+                    if(k == i || k == j) continue;
+
+                    if(digits[k] % 2 == 0) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        // If duplicate digits exist, duplicate numbers should only be counted once.
+        set<int> s;
+
+        for(int i = 0; i < n; i++) {
+            if(digits[i] == 0) continue;
+
+            for(int j = 0; j < n; j++) {
+                if(j == i) continue;
+
+                for(int k = 0; k < n; k++) {
+                    if(k == i || k == j) continue;
+
+                    if(digits[k] % 2 == 0) {
+                        int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+                        s.insert(num);
+                    }
+                }
+            }
+        }
+
+        return s.size();
+    }
+};
